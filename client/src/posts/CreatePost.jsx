043a163a92eacce78833c42d72dcbca8777e5f80
MatejKomlossy <matej.komlossy@gmail.com>
2022-11-23 20:14:23 +0100

    create post not working

--- client/src/posts/CreatePost.jsx
+++ client/src/posts/CreatePost.jsx
@@ -3,6 +3,11 @@ import InputField from "../components/InputField";
 import Button from "../components/Button";
 import Textarea from 'react-expanding-textarea';
 import DragDrop from "../components/DragDrop";
+import {postCreate} from "../constants/backendUrls";
+import {Navigate} from "react-router-dom";
+import {posts} from "../constants/frontendUrls";
+import useAlert from "../hooks/useAlrert";
+import Alert from "../components/Alert";
 
 
 function CreatePost({}) {
@@ -11,13 +16,50 @@ function CreatePost({}) {
     const [title, setTitle] = useState("");
     const [text, setText] = useState("");
     const [file, setFile] = useState(null);
+    const [wasCreated, setWasCreated] = useState(false);
+
+    const [showAlert, setShowAlert,
+        alertType, setAlertType,
+        alertTitle, setAlertTitle,
+        alertContext, setAlertContext] = useAlert();
 
     useEffect(() => {
         textareaRef.current.focus()
     }, [])
 
+    const create = () => {
+        const post = {"title": title, "post_text": text, "flag": true};
+        const req = fetch(postCreate, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({"post": post})
+        });
+        req.then(res => {
+            if (res.ok) {
+                setWasCreated(true)
+            } else {
+                res.json().then(data => showError(data.msg))
+            }
+        }).catch(err => {
+            showError(err)
+        })
+    }
+
+    const showError = (errorMessage) => {
+        setShowAlert(true);
+        setAlertType('error');
+        setAlertTitle("Error");
+        setAlertContext(`${errorMessage}`)
+    }
+
+    if (wasCreated) {
+        return <Navigate to={posts}/>
+    }
+
     return (
         <div className="border-2 border-blue-600 rounded-xl p-6 w-10/12 min-w-max mx-auto">
+            {showAlert && <Alert type={alertType} title={alertTitle} context={alertContext}/>}
+
             <div className="flex flex-col mx-auto gap-6">
                 <InputField type={'text'}
                             label={'Title'}
@@ -39,7 +81,11 @@ function CreatePost({}) {
                 <div className="flex">
                     <DragDrop file={file} setFile={setFile}/>
                     <div className="flex-none w-30 h-14 ml-auto">
-                        <Button onClick={() => console.log("Post")} type={'primary'} children={'Post'}/>
+                        <Button
+                            type={'primary'}
+                            onClick={() => create()}
+                            children={'Post'}
+                        />
                     </div>
                 </div>
             </div>
