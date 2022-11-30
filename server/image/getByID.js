4975160b1ec3c5b28f0b741463f975aa732b2b25
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2022-11-30 15:50:03 +0100

    img to json

--- server/image/getByID.js
+++ server/image/getByID.js
@@ -10,11 +10,10 @@ function preImageGetOneID(){
             if (canContinue(req, res, keys, req.body)===false) {
                 return;
             }
-            res.writeHead(200,{'content-type':('image/'+req.body.mextname)});
-            fs.createReadStream(
-                Path.join(savePath,req.body.id+'.'+req.body.mextname)
-            ).pipe(res);
-
+            res.status(200).json({
+            img:fs.readFileSync(Path.join(savePath,req.body.id+'.'+req.body.mextname),
+                {encoding:'utf8', flag:'r'})
+            });
         } catch (e) {
             res.status(500).send(e.toString());
         }
