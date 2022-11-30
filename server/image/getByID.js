74996c44639e6d07cbb8406fcfc8fdb2b0044d5a
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2022-11-30 14:57:51 +0100

    other send image

--- server/image/getByID.js
+++ server/image/getByID.js
@@ -1,7 +1,6 @@
 const {canContinue} = require("../general/canContinue");
 const Path = require("path");
 const {savePath} = require("../contants/urlsPaths");
-const fs = require("fs");
 
 function preImageGetOneID(){
     return async function(req, res) {
@@ -11,10 +10,9 @@ function preImageGetOneID(){
                 return;
             }
             res.writeHead(200,{'content-type':('image/'+req.body.mextname)});
-            fs.createReadStream(
+            res.sendFile(
                 Path.join(savePath,req.body.id+'.'+req.body.mextname)
-            ).pipe(res);
-
+            );
         } catch (e) {
             res.status(500).send(e.toString());
         }
